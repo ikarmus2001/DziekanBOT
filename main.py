2690c92e37e4e54424569e9eeca68c577ef1eb64
ikarmus2001 <kacpiczek@gmail.com>
2021-01-31 19:22:02 +0100

    changing prefix works, color on //me depends on highest role color

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ class BOT(dc.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.prefix = cfg['prefix']
+        # print(type(cfg))
 
     async def on_ready(self):
         for guild in self.guilds:
@@ -32,39 +33,67 @@ class BOT(dc.Client):
         if content == "hi":
             await message.reply("hi!")
 
-        if content.startswith("avatar") or content.startswith("av"):
+        elif content.startswith("avatar") or content.startswith("av"):
             avatar_url = self.getAvatarURL(message.author)
-            await message.send(message.reply(avatar_url))
+            await message.channel.send(message.reply(avatar_url))
 
         elif content.startswith("me"):
-            await self.me_richtext(message, message.author)  # message.author
+            await self.me_richtext(message, message.author)
+
+        elif content.startswith("prefix"):
+            content_func = content[7:]  # uwzględniam spację
+            print(content_func)
+            if content_func.startswith("change"):
+                new_prefix = content_func.split(" ")[-1]
+                await message.channel.send("Zmieniasz prefix na {}".format(new_prefix))
+                print(new_prefix)
+                await message.channel.send(await bot_client.prefix_change(new_prefix=new_prefix))
+            else:
+                await message.channel.send("Aktualny prefix: {}".format(cfg["prefix"]))
 
     def getAvatarURL(self, user):
         base = "https://cdn.discordapp.com/avatars/"
         return base + str(user.id) + "/" + str(user.avatar)
 
-    async def me_richtext(self, message, user, title="Title", color=0x00ff00, desc="Short description"):
-        embd = dc.Embed(title=title, description=desc, color=color)
+    async def me_richtext(self, message, user, title="Title", color=0xff0000, desc=None):
+        embd = dc.Embed(title=title)
         embd.set_image(url=self.getAvatarURL(user))
 
-        joinDateDate = user.joined_at
-        joinDateString = joinDateDate.strftime("%d, %m, %Y")  # when user created profile
-        dayAmount = str(datetime.datetime.now() - joinDateDate)
-        joinValue = "Jesteś z nami już od {}, \nco daje w sumie {}!".format(joinDateString, dayAmount)
+        if desc:
+            embd.description(desc)
+
+        join_date_date = user.joined_at
+        join_date_string = join_date_date.strftime("%Y, %m, %d")  # when user created profile
+        day_amount = str(datetime.datetime.now() - join_date_date)
+        join_value = "Jesteś z nami już od {}, \nco daje w sumie {}!".format(join_date_string, day_amount)
 
         roles = []
         for x in user.roles:
             if x.name != '@everyone':
                 roles.append(x.mention)
         if len(roles) == 0:
-            roles = "Nikt Cię nie wtajemniczył?" # Napisz do {}!".format(dc.Role.mention) wyciąganie z bazy
+            roles = "Nikt Cię nie wtajemniczył?"  # Napisz do {}!".format(dc.Role.mention) wyciąganie z bazy
         else:
             roles.reverse()
+            color = user.color
             roles = ", ".join(roles)
+        embd.color = color
 
-        embd.add_field(name="Dołączenie", value=joinValue, inline=False)
+        embd.add_field(name="Dołączenie", value=join_value, inline=False)
         embd.add_field(name="Twoje role:", value=roles, inline=False)
+        # embd.add_field(name=)
         await message.channel.send(embed=embd)
+
+    async def prefix_change(self, new_prefix):
+        print("Changing prefix - entering method")
+        cfg["prefix"] = new_prefix
+        self.prefix = new_prefix
+        f = open("config.json", "w")
+        json.dump(cfg, f)
+        f.close()
+        result = "Prefix changed to {}, exiting method".format(new_prefix)  # by someone
+        print(result)
+        return result
 
 
 bot_client = BOT()
