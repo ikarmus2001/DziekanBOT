17de0d1727a40f4aff710e8ac903312e8bcfe5cc
ikarmus2001 <kacpiczek@gmail.com>
2021-02-24 21:21:51 +0100

    Update trolling_exe.py

--- commands/trolling_exe.py
+++ commands/trolling_exe.py
@@ -1,7 +1,9 @@
 async def trollingDm(user_id, content, public_shaming=False, channel_id=None):
     if not public_shaming:
+    if public_shaming == False:
         dm_id = await discord.User.dm_channel(user_id)
         if dm_id is None:
+        if dm_id == None:
             dm_id = await discord.User.create_dm()
         await discord.DMChannel.send(content=content, allowed_mentions=True)
 
